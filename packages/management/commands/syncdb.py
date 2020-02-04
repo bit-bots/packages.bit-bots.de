a8fdf194e692fa9729900464ba01bc532fdf961b
Timon Engelke <timon.engelke@online.de>
2020-02-04 16:47:44 +0100

    improve query handling in syncdb

--- packages/management/commands/syncdb.py
+++ packages/management/commands/syncdb.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.db.models import ObjectDoesNotExist
@@ -10,40 +12,23 @@ class Command(BaseCommand):
     help = 'Synchronizes the database with the local package archive'
 
     def handle(self, *args, **kwargs):
-        self._create_upstream_packages()
-        self._update_local_packages()
-
-    def _update_local_packages(self):
-        packages = parse_release_file(settings.LOCAL_URL)
-        for package_name in packages:
-            try:
-                package = Package.objects.get(name=package_name)
-                upstream_version = package.version
-                local_version = packages[package_name]
-                if upstream_version == local_version:
-                    package.upstream_state = UpstreamState.UP_TO_DATE
-                    package.local_state = LocalState.UP_TO_DATE
-                elif upstream_version > local_version:
-                    package.upstream_state = UpstreamState.UPDATE_AVAILABLE
-                    package.local_state = LocalState.QUEUED
-                    package.version = packages[package_name]
+        upstream_packages = parse_release_file(settings.UPSTREAM_URL)
+        local_packages = Package.objects.in_bulk(field_name='name')  # type: Dict[str, Package]
+        to_create = []
+        for package_name in upstream_packages:
+            if package_name in local_packages.keys():
+                # Package in local packages -> Check for upstream version
+                if upstream_packages[package_name] == local_packages[package_name].version:
+                    local_packages[package_name].upstream_state = UpstreamState.UP_TO_DATE
+                elif upstream_packages[package_name] <= local_packages[package_name].version:
+                    print(f'Package {package_name} newer than upstream package')
                 else:
-                    print(f'Error: Package {package_name} newer than upstream')
-                    package.upstream_state = UpstreamState.UNKNOWN
-                package.save()
-            except ObjectDoesNotExist:
-                print(f'Error: Package {package_name} does not exist in upstream')
-
-    def _create_upstream_packages(self):
-        packages = parse_release_file(settings.UPSTREAM_URL)
-        for package_name in packages:
-            try:
-                package = Package.objects.get(name=package_name)
-                print(f'Updating package {package_name}')
-                package.version = packages[package_name]
-                package.upstream_state = UpstreamState.ONLY_UPSTREAM
-                package.save()
-            except ObjectDoesNotExist:
-                print(f'Adding package {package_name} {packages[package_name]}')
-                Package.objects.create(name=package_name, version=packages[package_name],
-                                       upstream_state=UpstreamState.ONLY_UPSTREAM)
+                    # Update available, put into queue
+                    local_packages[package_name].upstream_state = UpstreamState.UPDATE_AVAILABLE
+                    local_packages[package_name].local_state = LocalState.QUEUED
+            else:
+                # Package not in local packages -> Add
+                to_create.append(Package(name=package_name, version=upstream_packages[package_name],
+                                         upstream_state=UpstreamState.ONLY_UPSTREAM))
+        Package.objects.bulk_create(to_create)
+        Package.objects.bulk_update(local_packages.values(), ['local_state', 'upstream_state'])
