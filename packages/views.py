4efa05e169714d5c26ba9a56d10f27f282ad5b02
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-02-20 21:20:43 +0100

    New queries

--- packages/views.py
+++ packages/views.py
@@ -2,15 +2,17 @@ from django import urls
 from django.conf import settings
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
-from packages.models import Package, LocalState
+from packages.models import Package, LocalState, UpstreamState
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'packages/index.html', {
+        'available': Package.objects.filter(upstream_state=UpstreamState.UP_TO_DATE),
         'progress': Package.objects.filter(local_state=LocalState.IN_PROGRESS),
         'queue': Package.objects.filter(local_state=LocalState.QUEUED),
-        'packages': Package.objects.order_by('upstream_state', 'name'),
+        'unavailable': Package.objects.filter(Q(upstream_state=UpstreamState.ONLY_UPSTREAM) & ~Q(local_state=LocalState.IN_PROGRESS) & ~Q(local_state=LocalState.QUEUED)),
         'LOCAL_URL': settings.LOCAL_URL
     })
 
